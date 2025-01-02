dcf5302a0c318189d4ac1f81e78367449febba67
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-01 23:03:32 -0300

    chore: cleanup

--- accounts/src/main/java/com/eazybytes/accounts/exception/GlabalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exception/GlabalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.eazybytes.accounts.exception;
 
 import java.time.LocalDateTime;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 
 @ControllerAdvice
